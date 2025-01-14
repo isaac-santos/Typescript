3061ae62252d53118149fde8ac4585ee9361c950
isaac-santos <isaacconceicao26@gmail.com>
2025-01-14 20:24:11 -0300

    fiz a atividade de Aliases do typescript

---
+++ 002-Tipos/007-Aliases/index.ts
@@ -0,0 +1,14 @@
+// aliases = apelidos
+type alphanumeric = string | number;
+
+let dados: alphanumeric;
+let dados2: alphanumeric;
+//let dados2: string | number | never;
+
+dados = 1
+dados = "Dener"
+dados = true
+
+dados2 = 1
+dados2 = "Dener"
+dados2 = true
